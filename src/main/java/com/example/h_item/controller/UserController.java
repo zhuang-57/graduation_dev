de273da741f7ff394c7b69d75563bb68a18c047b
zhuang-57 <3033018047@qq.com>
2024-04-21 16:27:28 +0800

    fix

--- src/main/java/com/example/h_item/controller/UserController.java
+++ src/main/java/com/example/h_item/controller/UserController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Resource
